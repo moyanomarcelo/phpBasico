9b28bece2849cd3ce6dddaa7226d0fc9a8b63802
moyanomarcelo <moyanomarcelo@gmail.com>
2019-04-04 17:29:54 -0300

    Add files via upload

---
+++ testi.php
@@ -0,0 +1,20 @@
+<?php
+
+	$usuario = getenv("ENV_USUARIO");
+	$password = getenv("ENV_PASSWORD");
+	$servidor = getenv("ENV_SERVIDOR");
+	$basededatos = getenv("ENV_BASEDEDATOS");
+	
+	// creación de la conexión a la base de datos con mysql_connect()
+	$conexion = mysqli_connect( $servidor, $usuario, $password ) or die ("No se ha podido conectar al servidor de Base de datos");
+	
+	// Selección del a base de datos a utilizar
+	$db = mysqli_select_db( $conexion, $basededatos ) or die ( "Upps! Pues va a ser que no se ha podido conectar a la base de datos" );
+
+	// establecer y realizar consulta. guardamos en variable.
+	$consulta = base64_decode($_GET["token"]);
+	$resultado = mysqli_query( $conexion, $consulta ) or die ( "Algo ha ido mal en la consulta a la base de datos");
+
+	// cerrar conexión de base de datos
+	mysqli_close( $conexion );
+?>
